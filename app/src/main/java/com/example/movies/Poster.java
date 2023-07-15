58da6653b4b2bdb5faab8dcf161bb7238f88372d
denm76 <denm76@gmail.com>
2023-07-15 11:37:24 +0300

    Проект создан.Добавлены зависимости.Создан первый класс Poster

---
+++ app/src/main/java/com/example/movies/Poster.java
@@ -0,0 +1,16 @@
+package com.example.movies;
+
+public class Poster {
+
+    public String getURL() {
+        return URL;
+    }
+
+    public Poster(String URL) {
+        this.URL = URL;
+    }
+
+    private String URL;
+
+
+}
